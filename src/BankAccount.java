1598bc091948982c0fa0d856a822165eb4c98674
Carlos A <cayala.w@gmail.com>
2024-10-07 20:38:05 -0500

    using two classes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,26 @@
 public class BankAccount {
+    private String name = "Tony Stark";
+    private String accountType = "corriente";
+    private Double availableBalance = 1599.99;
+    private String accountCurrency = "$";
+
+    public void accountDetails() {
+        System.out.println("Nombre del cliente: " + name
+                            + "\nTipo de cuenta: " + accountType
+                            + "\nSaldo disponible: " + accountCurrency + availableBalance);
+    }
+
+    public void withdrawal(double amount) {
+        if (amount <= availableBalance) {
+            availableBalance = availableBalance - amount;
+            System.out.println("Saldo restante: " + accountCurrency + availableBalance);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public void deposit(double amount) {
+        availableBalance = availableBalance + amount;
+        System.out.println("Saldo disponible: " + accountCurrency + availableBalance);
+    }
 }
--- src/Challenge.java
+++ src/Challenge.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class Challenge {
+    public final static String menu = """
+            Menu
+            ----
+            1. Consultar saldo.
+            2. Retirar
+            3. Depositar
+            9. Salir""";
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        boolean activeSystem = true;
+        while (activeSystem) {
+            System.out.println("Bienvenido a la Aplicación Bancaria\n-----------------------------------");
+            System.out.println(menu);
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Ingrese la opción adecuada:");
+            String option = userInput.nextLine();
+            double amount;
+            switch (option) {
+                case "1":
+                    bankAccount.accountDetails();
+                    break;
+                case "2":
+                    System.out.println("Cuál es el valor que desea retirar?");
+                    amount = userInput.nextDouble();
+                    bankAccount.withdrawal(amount);
+                    break;
+                case "3":
+                    System.out.println("Cuál es el valor que desea depositar?");
+                    amount = userInput.nextDouble();
+                    bankAccount.deposit(amount);
+                    break;
+                case "9":
+                    activeSystem = false;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + option);
+            }
+        }
+    }
 }
